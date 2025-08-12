c63c559cc6b9d1004ed7dcd307f29e637c2eb581
Morris <itsmorriskiburi@gmail.com>
2025-08-12 15:28:55 +0300

    first commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,25 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name = 'cals', breed = "Mastiff"):
+        self.name = name
+        self.breed = breed 
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if not (isinstance(name, str)) or not 0 < len(name) < 25:
+            print("Name must be string between 1 and 25 characters.")
+            return 
+        self._name = name
+
+    @property
+    def breed(self):
+        return self._breed
+    @breed.setter
+    def breed(self, breed):
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            return
+        self._breed = breed
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,26 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = 'John', job= 'Sales'):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if not (isinstance(name, str)) or not 0 < len(name) < 25:
+            print("Name must be string between 1 and 25 characters.")
+            return
+        self._name = name.title()
+
+    @property
+    def job(self):
+        return self._job
+    @job.setter
+    def job(self, job):
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+            return
+        self._job = job
